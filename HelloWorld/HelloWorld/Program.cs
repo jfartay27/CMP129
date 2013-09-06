57a25dffd0282d797b16161b4c3411f8932514db
Joseph Forte <jfartay27@gmail.com>
2013-09-05 20:11:49 -0400

    initial commit!
    Signed-off-by: Joseph Forte <jfartay27@gmail.com>

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+           if (value == 0)
+	{
+	    return -1; 
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("3");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+            Console.WriteLine("6");
+            Console.WriteLine("7");
+            Console.WriteLine("8");
+            Console.WriteLine("9");
+            Console.WriteLine("10");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("11");
+            Console.WriteLine("12");
+            Console.WriteLine("13");
+            Console.WriteLine("14");
+            Console.WriteLine("15");
+            Console.WriteLine("16");
+            Console.WriteLine("17");
+            Console.WriteLine("18");
+            Console.WriteLine("19");
+            Console.WriteLine("20");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("21");
+            Console.WriteLine("22");
+            Console.WriteLine("23");
+            Console.WriteLine("24");
+            Console.WriteLine("25");
+            Console.WriteLine("26");
+            Console.WriteLine("27");
+            Console.WriteLine("28");
+            Console.WriteLine("29");
+            Console.WriteLine("30");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("31");
+            Console.WriteLine("32");
+            Console.WriteLine("33");
+            Console.WriteLine("34");
+            Console.WriteLine("35");
+            Console.WriteLine("36");
+            Console.WriteLine("37");
+            Console.WriteLine("38");
+            Console.WriteLine("39");
+            Console.WriteLine("40");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Brown;
+            Console.WriteLine("41");
+            Console.WriteLine("42");
+            Console.WriteLine("43");
+            Console.WriteLine("44");
+            Console.WriteLine("45");
+            Console.WriteLine("46");
+            Console.WriteLine("47");
+            Console.WriteLine("48");
+            Console.WriteLine("49");
+            Console.WriteLine("50");
+            Console.ResetColor();
+
+
+        }
+
+
+    }
+}
